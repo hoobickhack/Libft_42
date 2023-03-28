e6b612c281edd178cb0c8b5107848a59e92da7a1
Ilyes Souaidia <isouaidi@c3r1p4.42nice.fr>
2023-03-28 16:38:00 +0200

    2commit

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isouaidi <isouaidi@sudent.42nice.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 14:36:50 by isouaidi          #+#    #+#             */
+/*   Updated: 2023/03/28 15:26:57 by isouaidi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == ' '))
+	i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+	result = result * 10 + str[i] - 48;
+	i++;
+	}
+	return (result * sign);
+}
